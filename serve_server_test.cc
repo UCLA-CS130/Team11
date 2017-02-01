06edc7c548019b506ec6dbeb3bcc9b8dbe282633
stephenlink <stephenlinkk@me.com>
2017-02-01 15:26:01 -0800

    trying to add serve_server unit tests

---
+++ serve_server_test.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <fstream>
+#include "gtest/gtest.h"
+#include "config_parser.h"
+#include "serve_server.h"
+
+
+
+TEST(configParseTest, correctConfig)
+{
+  configArguments configArgs;
+  const char* argv[2] = {"./serve", "demo_config"};
+  int errorCode = Server::parseConfig(2, argv, configArgs);
+  EXPECT_EQ(errorCode, 0);
+  EXPECT_EQ(configArgs.port, 1111);
+
+}
+
+
