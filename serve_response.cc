b66dbbb7ebca231d0abbbe3d5626b7c43e6245bf
bubblynatalie <yummyraspberries@gmail.com>
2017-02-04 21:11:39 -0800

    Proper reponse in browser now

--- serve_response.cc
+++ serve_response.cc
@@ -1,23 +1,29 @@
-#include <boost/asio.hpp>
-#include <string>
-#include <vector>
+#include "serve_server.h"
 #include "serve_response.h"
 
+#include <iostream>
+#include <string>
 
-std::string Response::response_builder(std::string status, std::string content)
-{
-  std::string response = "";
-  response += status + crlf;
+using boost::asio::ip::tcp;
+
+// Read in the request from the socket used to construct the handler, then
+// fill the response buffer with the necessary headers followed by the
+// original request.
+std::string Response::response_builder(std::string status) {
+
+  std::string response_header = "";
+  response_header += status + crlf;
   for (unsigned int i = 0; i < headers.size(); ++i)
   {
     Header& h = headers[i];
-    response += h.name;
-    response += name_value_separator;
-    response += h.value;
-    response += crlf;
+    response_header += h.name;
+    response_header += name_value_separator;
+    response_header += h.value;
+    response_header += crlf;
   }
-  response += crlf;
-  response += content;
-  return response;
+  response_header += crlf;
+  
+  //std::size_t header_size = response_header.size();
+  return response_header;
 }
 
