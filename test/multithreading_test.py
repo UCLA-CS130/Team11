d7b902ebda6e8fb3e945167ed5459ada13dcb842
bubblynatalie <yummyraspberries@gmail.com>
2017-03-03 18:21:07 -0800

    Added echo requests

--- test/multithreading_test.py
+++ test/multithreading_test.py
@@ -18,4 +18,20 @@ serv = subprocess.Popen(["./serve", "new_config"])
 num_threads = int(sys.argv[1])
 print('Thread number : '+ str(num_threads))
 
+expected_response_first = """HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+\r
+GET /echo HTTP/1.1\r
+User-Agent: telnet"""
+
+expected_response_second = """Host: localhost:9999\r
+Accept: */*\r\n\r\n"""
+
+for request_index in range(num_threads):
+	echo_request = "curl -i localhost:9999/echo"
+	echo_request_process = subprocess.Popen(echo_request, stdout=subprocess.PIPE, shell=True)
+	echo_response = echo_request_process.stdout.read().decode('utf-8')
+	
+	print("Echo response: " + str(echo_response))
+
 serv.kill()
